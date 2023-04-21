744279e2ed212c9818262d7631868dc2ede6bfac
Soikat Rahman <hello.dev.sr@gmail.com>
2023-04-21 21:55:44 +0600

    cleaned

--- 11-next13/01-fullstack-with-13-v1/app/Posts.tsx
+++ 11-next13/01-fullstack-with-13-v1/app/Posts.tsx
@@ -6,7 +6,6 @@ import { Button } from '~/components/ui/button';
 import { Card, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
 import { cn } from '~/lib/utils';
 import { PostType } from '~/types/Post';
-import getQueryClient from './query/getQueryClient';
 
 const getPosts = async () => {
 	const { data } = await axios.get<PostType[]>('/api/posts');
@@ -25,7 +24,7 @@ const Posts = () => {
 	});
 	const queryClient = useQueryClient(); //not getQueryClient
 
-	// Vs: Normal Delete
+	// Normal Delete
 	const deletePost = async (id: string) => {
 		deleteHandler(id);
 		queryClient.invalidateQueries({ queryKey: ['posts'] });
@@ -38,7 +37,7 @@ const Posts = () => {
 		}
 	});
 
-	// Optimistic Delete
+	// Vs: Optimistic Delete
 	const deleteMutation2 = useMutation({
 		mutationFn: deleteHandler,
 		onMutate: async (id: string) => {
@@ -86,75 +85,3 @@ const Posts = () => {
 };
 
 export default Posts;
-
-// 'use client';
-
-// import { useMutation, useQuery } from '@tanstack/react-query';
-// import axios from 'axios';
-// import { Button } from '~/components/ui/button';
-// import { Card, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
-// import { cn } from '~/lib/utils';
-// import { PostType } from '~/types/Post';
-// import getQueryClient from './query/getQueryClient';
-
-// const getPosts = async () => {
-// 	const { data } = await axios.get<PostType[]>('/api/posts');
-// 	return data;
-// };
-// const deleteHandler = async (id: string) => {
-// 	const { data } = await axios.delete(`/api/posts/${id}`);
-// 	return data;
-// };
-
-// export default function Posts() {
-// 	const { data: posts, isLoading } = useQuery({
-// 		queryKey: ['posts'],
-// 		queryFn: getPosts
-// 	});
-
-// 	const queryClient = getQueryClient();
-// 	const deleteMutation = useMutation({
-// 		mutationFn: deleteHandler,
-// 		onMutate: async (id) => {
-// 			// Cancel any outgoing refetches (so they don't overwrite our optimistic update)
-// 			await queryClient.cancelQueries({ queryKey: ['posts'] });
-// 			// Snapshot the previous value
-// 			const previousPosts = queryClient.getQueryData<PostType[]>(['posts']);
-// 			// Optimistic update the data
-// 			if (previousPosts) {
-// 				queryClient.setQueryData<PostType[]>(
-// 					['posts'],
-// 					previousPosts.filter((post) => post.id !== id)
-// 				);
-// 			}
-// 			// Return the snapshotted value
-// 			return { previousPosts };
-// 		},
-// 		onError: (err, variables, context) => {
-// 			if (context?.previousPosts) {
-// 				queryClient.setQueryData(['posts'], context.previousPosts);
-// 			}
-// 		},
-// 		onSettled: () => {
-// 			queryClient.invalidateQueries({ queryKey: ['posts'] });
-// 		}
-// 	});
-
-// 	return (
-// 		<div>
-// 			{posts?.map((post) => (
-// 				<Card className={cn('w-full', '')} key={post.id}>
-// 					<CardHeader>
-// 						<CardTitle>{post.title}</CardTitle>
-// 						<CardDescription>{post.content}</CardDescription>
-// 						<div className='flex w-full justify-end'>
-// 							<Button variant='destructive' onClick={() => deleteMutation.mutate(post.id)}>
-// 								Delete
-// 							</Button>
-// 						</div>
-// 					</CardHeader>
-// 				</Card>
-// 			))}
-// 		</div>
-// 	);
-// }
